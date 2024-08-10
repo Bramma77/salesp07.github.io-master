0674add0275dc38bb36a8b0ad8ba8de333269fe4
Devbramma <developerapp375@gmail.com>
2024-08-10 12:50:30 +0530

    first

---
+++ src/components/Projects/ProjectCard/ProjectCard.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { ProjectList } from "../../../data/ProjectData";
+import {
+  Card,
+  CardLeft,
+  CardRight,
+  TechCardContainer,
+  TechCard,
+  BtnGroup,
+} from "./ProjectCardElements";
+import ScrollAnimation from "react-animate-on-scroll";
+import "./ProjectCard.css";
+function ProjectCard() {
+  return (
+    <>
+      {ProjectList.map((list, index) => (
+        <ScrollAnimation animateIn="fadeInLeft" key={index}>
+          {/* <Card> */}
+          <div className="card">
+            <div className="cardLeft">
+              <div style={{}}>
+                <img
+                  src={list.img}
+                  alt={list.name}
+                  style={{ borderRadius: 20 }}
+                />
+              </div>
+              <div>
+                <img
+                  src={list.img1}
+                  alt={list.name}
+                  style={{ borderRadius: 20 }}
+                />
+              </div>
+              <div>
+                <img
+                  src={list.img2}
+                  alt={list.name}
+                  style={{ borderRadius: 20 }}
+                />
+              </div>
+              <div>
+                <img
+                  src={list.img3}
+                  alt={list.name}
+                  style={{ borderRadius: 20 }}
+                />
+              </div>
+            </div>
+            <div className="cardRight">
+              <h4>{list.title}</h4>
+              <p>{list.description}</p>
+              <TechCardContainer>
+                {list.tech_stack.map((tech, index) => (
+                  <TechCard key={index}>{tech}</TechCard>
+                ))}
+              </TechCardContainer>
+
+              <BtnGroup>
+                {/* <a className="btn SecondaryBtn btn-shadow">Play Store</a>
+                <a className="btn PrimaryBtn btn-shadow">Demo ➜</a> */}
+                {(list.title === "VisDoc" ||
+                  list.title === "AllDine" ||
+                  list.title === "Laxmi Astro Ai") && (
+                  <a
+                    className="btn1 SecondaryBtn1 btn-shadow"
+                    href={list.github_url}
+                  >
+                    <img className="imgs" src="./play.png" />
+                  </a>
+                )}
+                {list.title === "VisDoc" && (
+                  <a className="btn1 SecondaryBtn1 btn-shadow">
+                    <img className="imgs" src="./appstore.jpg" />
+                  </a>
+                )}
+
+                {/* {list.github_url.length > 0 && (
+                  <a
+                    className="btn SecondaryBtn btn-shadow"
+                    href={list.github_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Github
+                  </a>
+                )} */}
+                {/* {list.demo_url.length > 0 && (
+                  <a
+                    className="btn PrimaryBtn btn-shadow"
+                    href={list.demo_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Demo ➜
+                  </a>
+                )} */}
+              </BtnGroup>
+            </div>
+          </div>
+          {/* </Card> */}
+        </ScrollAnimation>
+      ))}
+    </>
+  );
+}
+
+export default ProjectCard;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,158 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/vis1.png",
+    img1: "/vis2.png",
+    img2: "/vis3.png",
+    img3: "/vis4.png",
+    title: "VisDoc",
+    description:
+      "The Medical Referral Management App is a versatile tool designed to streamline scheduling and managing patient referrals across various medical roles and facilities. This app ensures efficient communication and coordination between clinicians, scribes, and healthcare providers, enhancing patient care and operational efficiency.",
+    tech_stack: ["React Native"],
+    github_url:
+      "https://play.google.com/store/apps/details?id=com.visdoc&hl=en_US",
+    demo_url: "https://play.google.com/store/apps/details?id=com.visdoc&hl=en",
+    appstore_url: "https://apps.apple.com/us/app/visdoc/id6468866718",
+  },
+  {
+    img: "/Astro1.png",
+    img1: "/Astro2.png",
+    img2: "/Astro3.png",
+    img3: "/Astro4.png",
+    img4: "/Astro5.png",
+
+    title: "Laxmi Astro Ai",
+    description:
+      "The Laxmi astro Ai is open ai based app to provide Intractive responses to astrology related questions.The app offers personalized features such as birth chart analysis,panchang details matchmaking option ",
+    tech_stack: ["React Native", "Firebase"],
+    github_url:
+      "https://play.google.com/store/apps/details?id=com.Laxmiastrology",
+    demo_url: "https://renr-bcit.web.app/",
+  },
+  {
+    img: "/all.png",
+    img1: "/all1.png",
+    img2: "/all4.png",
+    img3: "/all3.png",
+    img4: "/all4.png",
+    title: "AllDine",
+    description:
+      "The App is food ordering app.Developed an app that offers both dine-in and take-away options.User can place orders for food to be enjoyed at the restaurant or request a parcel for take away",
+    tech_stack: ["React-Native", "Firebase"],
+    github_url:
+      "https://play.google.com/store/apps/details?id=com.alldine.hotel",
+    demo_url: "https://mymindweb.vercel.app/",
+  },
+  {
+    img: "/con1.png",
+    img1: "/con2.png",
+    img2: "/con3.png",
+    img3: "/con5.png",
+    img4: "/con5.png",
+    title: "Construction App",
+    description:
+      "Effiecently manage your construction Projects.Track and budget manpower cost,material expenses,and overall project spending monitor the remaining budget to ensure stay track.Generate invoices and quotations seamlessly to streamline your financial Process and keeps everything organized",
+    tech_stack: ["React-Native", "Firebase"],
+    github_url: "",
+    demo_url: "https://pokedex-salesp07.netlify.app/",
+  },
+  {
+    img: "/tile2.png",
+    img1: "/tile1.png",
+    title: "Tiles Calculator ",
+    description:
+      "An app to detect if a job posting is a scam or not using its description.",
+    tech_stack: ["React-Native"],
+    github_url: "",
+    demo_url: "https://jobdetective.onrender.com/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+  //   name: "NextJs",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+    name: "Firebase",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+  //   name: "Python",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+  //   name: "Flask",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //   name: "Java",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+  //   name: "MySQL",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+  //   name: "Bootstrap",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+  //   name: "Material UI",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+  //   name: "Figma",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+  //   name: "TailwindCSS",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
